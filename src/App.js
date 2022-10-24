c9125d12dfbfc2e361c8ce72c63175ea1af6e4cd
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2022-10-24 18:14:06 +0800

    Project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
