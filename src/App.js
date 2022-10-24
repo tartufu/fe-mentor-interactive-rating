08547a8e117f70b4866553d66c7c15fd22442d16
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2022-10-24 18:24:33 +0800

    Box flex for skeleton layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,27 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            alignContent: "center",
+            height: "100vh",
+          }}
+        >
+          Hello World
+        </Box>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
